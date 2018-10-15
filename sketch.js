e045af9f0d664815a7917cb3f5d7de382e18e84c
maf <mafffia@qq.com>
2018-10-15 21:42:10 +0800

    01

--- sketch.js
+++ sketch.js
@@ -1,51 +1,39 @@
 function setup() {
-  createCanvas(500, 500)
-  noStroke()
+  createCanvas(300, 300)
+  background(0)
+  noSmooth()
 }
 
 function draw() {
-  //   background("Pink")
-  for (var y = 0; y <= height; y = y + 40) {
-    for (var x = 0; x <= width; x = x + 40) {
-      fill(255, 12, 255, 1)
-      ellipse(x, y, 40, 40)
+  //   background(0)
+  for (var y = 20; y <= height - 20; y += 10) {
+    // height-20 -> in the CENTER
+    for (var x = 20; x <= width - 20; x += 10) {
+      // Draw a line to the center of the display
+      stroke(244, 96, 54, 150)
+      var a = random(20, height - 20)
+      var b = random(20, width - 20)
+      line(x, y, a, b)
+      noStroke()
+      fill(255)
+      ellipse(x, y, 4, 4)
     }
   }
-  //   fill("Teal")
-  //   textSize(100)
-  //   textAlign(CENTER)
-  //   text("Eric", width / 2, height / 2)
-  //   text("Liang", width / 2, height / 2 + 80)
+  // arc(90, 250, 80, 80, 0, HALF_PI)
+  arc(width / 2, height / 2, 80, 80, 0, PI + HALF_PI)
+  //   arc(290, 250, 80, 80, PI, TWO_PI + HALF_PI)
+  // arc(390, 250, 80, 80, QUARTER_PI, PI + QUARTER_PI)
 
-  for (var y = 80; y <= 440; y = y + 40) {
-    fill(255, 12, 255, 3)
-    ellipse(240, y, 40, 40)
-  } // middle 1
-
-  for (var y = 160; y <= 320; y = y + 40) {
-    fill(255, 12, 255, 3)
-    ellipse(120, y, 40, 40)
-  } // left 1
-  for (var y = 160; y <= 320; y = y + 40) {
-    fill(255, 12, 255, 3)
-    ellipse(360, y, 40, 40)
-  } // right 1
-
-  for (var x = 120; x <= 360; x = x + 40) {
-    fill(255, 12, 255, 3)
-    ellipse(x, 160, 40, 40)
-  } // up 1
-  for (var x = 120; x <= 360; x = x + 40) {
-    fill(255, 12, 255, 3)
-    ellipse(x, 320, 40, 40)
-  } // down 1
-
-  var a = random(0, 500)
-  var b = random(0, 500)
-  fill(255)
-  ellipse(a * 2, b, 40, 40)
-  fill(211, 212, 233)
-  ellipse(a, b * 3, 40, 40)
-  fill("Lightblue")
-  ellipse(a / 2, b, 40, 40)
+  if (frameCount % 15 === 0) {
+    // frameCount 除以 60 的余数，为 0 -> 被整除
+    clear()
+    background(0)
+  }
+  if (frameCount % 60 === 0) {
+    // frameCount 除以 60 的余数，为 0 -> 被整除
+    ellipse(0, 0, 300, 300)
+    ellipse(0, 300, 300, 300)
+    ellipse(300, 0, 300, 300)
+    ellipse(300, 300, 300, 300)
+  }
 }
