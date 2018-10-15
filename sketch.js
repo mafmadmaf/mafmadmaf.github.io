6a9c5b85871f7c558a90245597b4e2ae9930e8ef
maf <mafffia@qq.com>
2018-10-15 16:53:04 +0800

    01

--- sketch.js
+++ sketch.js
@@ -11,11 +11,34 @@ function draw() {
       ellipse(x, y, 40, 40)
     }
   }
-  fill("Teal")
-  textSize(100)
-  textAlign(CENTER)
-  text("Eric", width / 2, height / 2)
-  text("Liang", width / 2, height / 2 + 80)
+  //   fill("Teal")
+  //   textSize(100)
+  //   textAlign(CENTER)
+  //   text("Eric", width / 2, height / 2)
+  //   text("Liang", width / 2, height / 2 + 80)
+
+  for (var y = 80; y <= 440; y = y + 40) {
+    fill(0)
+    ellipse(240, y, 40, 40)
+  } // middle 1
+
+  for (var y = 160; y <= 320; y = y + 40) {
+    fill(0)
+    ellipse(120, y, 40, 40)
+  } // left 1
+  for (var y = 160; y <= 320; y = y + 40) {
+    fill(0)
+    ellipse(360, y, 40, 40)
+  } // right 1
+
+  for (var x = 120; x <= 360; x = x + 40) {
+    fill(0)
+    ellipse(x, 160, 40, 40)
+  } // up 1
+  for (var x = 120; x <= 360; x = x + 40) {
+    fill(0)
+    ellipse(x, 320, 40, 40)
+  } // down 1
 
   var a = random(0, 10)
   var b = random(0, 10)
